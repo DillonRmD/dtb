59124fc7fb9172a158d8671f155b81c977e510a6
Dillon Williams <dillonwilliams2001@yahoo.com>
2019-04-08 13:39:28 -0500

    Updated dtb_gl.c

--- src/dtb_test.cpp
+++ src/dtb_test.cpp
@@ -15,6 +15,18 @@ int main(int argc, char* argv[])
 		0.0f, 0.5f,
 	};
 	
+	uint program = dtbgl_create_shaders("#version 330 core\n", 
+										
+										"layout(location = 0) in vec3 pos;\n"
+										"void main(){\n"
+										"gl_Position.xyz = pos;\n"
+										"gl_Position.w = 1.0;}",
+										
+										"out vec3 color;\n"
+										"void main(){\n"
+										"color = vec3(0, 1, 0);\n"
+										"}");
+	
 	uint buffer;
 	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
@@ -23,14 +35,17 @@ int main(int argc, char* argv[])
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(0);
 	
+	
+	
 	while(platform.running)
 	{
 		dtb_platform_update();
 		
+		glUseProgram(program);
+		
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		
 		printf("X: %i, Y: %i\n", platform.cursorX, platform.cursorY);
-		
 	}
 	
 	return 0;
